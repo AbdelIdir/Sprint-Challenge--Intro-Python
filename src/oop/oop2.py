241612c155f9f7911c1c1f06569d04b1c13951ca
AbdelI <homeworkoracle@protonmail.com>
2020-04-10 09:27:36 +0200

    Printing the result for each function drive() call

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -28,6 +28,7 @@ class Motorcycle(GroundVehicle):
 
 # TODO
 
+
 vehicles = [
     GroundVehicle(),
     GroundVehicle(),
@@ -38,4 +39,5 @@ vehicles = [
 
 # Go through the vehicles list and print the result of calling drive() on each.
 
-# TODO
+for vehicle in vehicles:
+    print(vehicle.drive())
