3f43694284b40b8024c13f7f8b247b2ade073dd6
AbdelI <homeworkoracle@protonmail.com>
2020-04-10 09:35:07 +0200

    Made list comprehension that returns all ages + 10

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -46,7 +46,7 @@ print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-d = []
+d = [dude.age + 10 for dude in humans]
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
